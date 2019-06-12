039adc25b38c874d855fd9bdf2323b7ad767460b
Geno Salvati <geno-github@users.noreply.github.com>
2019-06-11 20:33:26 -0500

    fixed issue with missed MSAL cache

--- HybridAuthCustomApp_V1/src/app/auth/auth-interceptor.service.ts
+++ HybridAuthCustomApp_V1/src/app/auth/auth-interceptor.service.ts
@@ -1,28 +1,42 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse, HttpErrorResponse }
+  from '@angular/common/http';
 
-import { environment } from '../../environments/environment';
+import { AuthMsalError } from './auth-msal-error';
 
+import { Observable } from 'rxjs/Observable';
+import { from } from 'rxjs';
+import 'rxjs/add/operator/do';
+import { mergeMap } from 'rxjs/operators';
 import { BroadcastService } from '../../vendor-modified-dist/msal-angular/broadcast.service';
 import { MsalService } from '../../vendor-modified-dist/msal-angular/msal.service';
-import { from, of } from 'rxjs';
-import { tap, mergeMap, switchMap, catchError } from 'rxjs/operators'
-import { Observable } from 'rxjs/Observable';
-
-// TODO: Remove when MSAL library supports Angular 6, and use MsalInterceptor instead
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor(private auth: MsalService, private broadcastService: BroadcastService) { }
+  constructor(
+    private auth: MsalService,
+    private broadcastService: BroadcastService
+  ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const scopes = this.auth.getScopesForEndpoint(req.url);
+    let dateNow = new Date();
+    console.log('AuthInterceptor intercept started at: ' + dateNow);
+    var scopes = [];
+    if (req.url.startsWith(`${environment.apiRoot}`) || req.url.startsWith('/api/')) {
+      scopes = [
+        `${environment.msalConfig.apiAppIdUri}`
+      ];
+    }
+    else {
+      scopes = this.auth.getScopesForEndpoint(req.url);
+    }
     this.auth.verbose('Url: ' + req.url + ' maps to scopes: ' + scopes);
     if (scopes === null) {
       return next.handle(req);
     }
-    const tokenStored = this.auth.getCachedTokenInternal(scopes);
+    var tokenStored = this.auth.getCachedTokenInternal(scopes);
     if (tokenStored && tokenStored.token) {
       req = req.clone({
         setHeaders: {
@@ -30,40 +44,53 @@ export class AuthInterceptorService implements HttpInterceptor {
         }
       });
       return next.handle(req).do(event => { }, err => {
-        if (err instanceof HttpErrorResponse && err.status === 401) {
-          const scopesl = this.auth.getScopesForEndpoint(req.url);
-          const tokenStoredl = this.auth.getCachedTokenInternal(scopesl);
-          if (tokenStoredl && tokenStoredl.token) {
-            this.auth.clearCacheForScope(tokenStoredl.token);
+        if (err instanceof HttpErrorResponse && err.status == 401) {
+          var scopes = this.auth.getScopesForEndpoint(req.url);
+          var tokenStored = this.auth.getCachedTokenInternal(scopes);
+          if (tokenStored && tokenStored.token) {
+            this.auth.clearCacheForScope(tokenStored.token);
           }
-          this.broadcastService.broadcast('msal:notAuthorized', { err, scopesl });
+          var msalError = new AuthMsalError(JSON.stringify(err), "", JSON.stringify(scopes));
+          this.broadcastService.broadcast('msal:notAuthorized', msalError);
         }
       });
-    } else {
-      return from(this.auth.acquireTokenSilent(scopes, environment.msalConfig.authority))
-        .pipe(
-          switchMap(token => {
-            const headers = req.headers
-              .set('Authorization', 'Bearer ' + token)
-            const requestClone = req.clone({
-                headers
-              });
-            return next.handle(requestClone).do(
-              event => {
-                console.log('next.handle event:', event);
-              },
-              err => {
-                if (err instanceof HttpErrorResponse && err.status === 401) {
-                  const scopesl = this.auth.getScopesForEndpoint(req.url);
-                  const tokenStoredl = this.auth.getCachedTokenInternal(scopesl);
-                  if (tokenStoredl && tokenStoredl.token) {
-                    this.auth.clearCacheForScope(tokenStoredl.token);
-                  }
-                  this.broadcastService.broadcast('msal:notAuthorized', { err, scopesl });
-                }
-              });
-          })
-        );
     }
+    else {
+      return from(this.auth.acquireTokenSilent(scopes).then(token => {
+        const JWT = `Bearer ${token}`;
+        return req.clone({
+          setHeaders: {
+            Authorization: JWT,
+          },
+        });
+      })).pipe(mergeMap(req => next.handle(req).do(event => { }, err => {
+        if (err instanceof HttpErrorResponse && err.status == 401) {
+          var scopes = this.auth.getScopesForEndpoint(req.url);
+          var tokenStored = this.auth.getCachedTokenInternal(scopes);
+          if (tokenStored && tokenStored.token) {
+            this.auth.clearCacheForScope(tokenStored.token);
+          }
+          var msalError = new AuthMsalError(JSON.stringify(err), "", JSON.stringify(scopes));
+          this.broadcastService.broadcast('msal:notAuthorized', msalError);
+        }
+      }))); //calling next.handle means we are passing control to next interceptor in chain
+    }
+    dateNow = new Date();
+    console.log('AuthInterceptor intercept completed at: ' + dateNow);
+
+    //intercept(
+    //    req: HttpRequest<any>,
+    //    next: HttpHandler
+    //): Observable<HttpEvent<any>> {
+
+    //    return next.handle(req).do(evt => {
+    //        if (evt instanceof HttpResponse) {
+    //            console.log('---> status:', evt.status);
+    //            console.log('---> filter:', req.params.get('filter'));
+    //        }
+    //    });
+
+    //}
   }
+
 }
